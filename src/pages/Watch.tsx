430e7b9d218a3b2ea06dfb78e108eea37d4812a4
lonelil <51315646+lonelil@users.noreply.github.com>
2022-11-29 05:55:21 +0800

    added like + dislikes and description and comments

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -7,7 +7,7 @@ import Hls from "hls.js";
 export default function Watch() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [video, setVideo] = useState<any>({});
-
+  const [comments, setComments] = useState<any[]>([]);
   const plyrRef = useRef<APITypes>(null);
 
   useEffect(() => {
@@ -21,7 +21,15 @@ export default function Watch() {
         hls.loadSource(data.hls);
         hls.attachMedia(video);
         //@ts-ignore
-        plyrRef.current.plyr.play();
+        //plyrRef.current.plyr.play();
+      });
+  }, [searchParams]);
+
+  useEffect(() => {
+    fetch(`https://pipedapi.kavin.rocks/comments/${searchParams.get("v")}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setComments(data.comments);
       });
   }, [searchParams]);
 
@@ -35,11 +43,11 @@ export default function Watch() {
                 {/*
                 // @ts-ignore */}
                 <Plyr ref={plyrRef} />
-                <h1 className="text-2xl font-bold text-white mt-2">
+                <h1 className="text-2xl font-bold text-white mt-2 mx-9 sm:mx-0">
                   {video.title ?? ""}
                 </h1>
 
-                <div className="flex items-center gap-3 mt-2">
+                <div className="flex items-center gap-3 my-2 mx-9 sm:mx-0">
                   <img
                     className="w-12 h-12 rounded-full"
                     src={video.uploaderAvatar ?? ""}
@@ -67,10 +75,123 @@ export default function Watch() {
                         )}
                       </strong>
                     </Link>
-                    <span className="text-slate-500 font-medium dark:text-slate-400">
+                    <span className="text-slate-500 font-light dark:text-slate-400">
                       {(video.uploaderSubscriberCount ?? "") + " Subscribers"}
                     </span>
                   </div>
+                  <div className="ml-auto">
+                    <div className="tabs tabs-boxed">
+                      <a className="tab">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+                          />
+                        </svg>
+                        &nbsp;
+                        {video.likes ?? ""}
+                      </a>
+                      <a className="tab">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
+                          />
+                        </svg>
+                        &nbsp;
+                        {video.dislikes ?? ""}
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="divider"></div>
+                <div className="flex flex-col w-auto lg:flex-row mx-9 sm:mx-0 mb-2">
+                  <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center p-3 overflow-scroll">
+                    <span className="font-bold">
+                      {(video.views ?? "") +
+                        " Views • " +
+                        (video.uploadDate ?? "")}
+                    </span>
+                    {video.description ?? ""}
+                  </div>
+                </div>
+                <div className="divider"></div>
+                <div className="mx-9 sm:mx-0">
+                  {comments?.map((comment: any) => (
+                    <div className="flex items-center gap-3 p-3">
+                      <img
+                        className="w-12 h-12 rounded-full"
+                        src={comment.thumbnail}
+                        loading="lazy"
+                      ></img>
+                      <div className="flex flex-col">
+                        <strong className="text-slate-900 font-medium dark:text-slate-200">
+                          {comment.author}
+                          <span className="text-gray-600 font-light">
+                            {" "}
+                            {comment.commentedTime}
+                          </span>
+                        </strong>
+                        <span className="text-slate-500 font-medium dark:text-slate-400">
+                          {comment.commentText}
+                        </span>
+                        <div className="tabs">
+                          <a className="tab">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-6 h-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+                              />
+                            </svg>
+                            &nbsp;
+                            {comment.likeCount ?? ""}
+                          </a>
+                          <a className="tab">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-6 h-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                              />
+                            </svg>
+                            &nbsp;
+                            {comment.replyCount ?? ""}
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
