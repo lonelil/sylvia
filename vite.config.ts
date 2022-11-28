ffe947edb3eb3a7f503dbc9f6473d89dda5d43d1
lonelil <51315646+lonelil@users.noreply.github.com>
2022-11-28 19:59:15 +0800

    added cabin font

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin(), webfontDownload()],
 });
