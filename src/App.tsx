4dbb3a1081c4cca7274c3066ab26d9b7b78e5a19
lonelil <51315646+lonelil@users.noreply.github.com>
2022-11-28 20:02:29 +0800

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
