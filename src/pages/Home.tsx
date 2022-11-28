c1d790a5a4692bb89b7dffd5f01098a2a784919f
lonelil <51315646+lonelil@users.noreply.github.com>
2022-11-28 21:27:29 +0800

    added home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,49 @@
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  const [videos, setVideos] = useState<any[]>([]);
+  useEffect(() => {
+    fetch("https://vid.puffyan.us/api/v1/popular")
+      .then((res) => res.json())
+      .then((data) => {
+        setVideos(data);
+      });
+  }, []);
+
+  console.log(videos);
   return (
-    <div>
-      <h1 className="text-8xl">Home</h1>
-    </div>
+    <>
+      <div className="grid w-full grid-cols-4 gap-4 px-6 justify-center items-center py-5">
+        {videos &&
+          videos.map((video, i) => (
+            <>
+              <div
+                className="card card-compact h-80 bg-secondary shadow-xl"
+                key={i}
+              >
+                <figure>
+                  <img
+                    src={`https://img.youtube.com/vi/${video.videoId}/hqdefault.jpg`}
+                    alt="Video"
+                    loading="lazy"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title truncate text-ellipsis overflow-hidden">
+                    {video.title}
+                  </h2>
+                  <p>
+                    <span>{video.author}</span>
+                    <br />
+                    <span>
+                      {video.viewCount} views • {video.publishedText}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </>
+          ))}
+      </div>
+    </>
   );
 }
