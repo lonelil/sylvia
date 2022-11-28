fb8c775606002fcd22b46e3ea60d39cceafb4049
lonelil <51315646+lonelil@users.noreply.github.com>
2022-11-29 00:54:46 +0800

    improve video page

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, Link } from "react-router-dom";
 import Plyr from "plyr-react";
 import "plyr-react/plyr.css";
 
@@ -18,31 +18,68 @@ export default function Watch() {
         );
       });
   }, [searchParams]);
-  console.log(videoStreams);
 
   return (
     <>
-      <section className="h-screen overflow-hidden">
-        <div className="container mx-auto px-4">
-          <div className="-m-6 flex flex-wrap justify-between pt-28 pb-40">
-            <div className="w-full lg:w-5/12 xl:w-1/2">
-              <div className="">
-                {videoStreams[0] ? (
-                  <Plyr
-                    source={{
-                      type: "video",
-                      sources: [
-                        {
-                          src: videoStreams.at(-1).url,
-                          type: "video/mp4",
-                        },
-                      ],
-                    }}
-                  />
-                ) : (
-                  <h1>loading...</h1>
-                )}
+      <section className="h-full min-h-screen overflow-hidden">
+        <div className="container mx-auto px-2">
+          <div className="-m-6 flex flex-wrap justify-between pt-14 pb-40">
+            <div className="w-full lg:w-8/12">
+              <div>
+                <Plyr
+                  source={{
+                    type: "video",
+                    sources: [
+                      {
+                        src: videoStreams[0]?.url ?? "",
+                        type: "video/mp4",
+                      },
+                    ],
+                  }}
+                />
+                <h1 className="text-2xl font-bold text-white mt-2">
+                  {video.title ?? ""}
+                </h1>
+
+                <div className="flex items-center gap-3 mt-2">
+                  <img
+                    className="w-12 h-12 rounded-full"
+                    src={video.authorThumbnails?.at(-1).url ?? ""}
+                  ></img>
+                  <div className="flex flex-col">
+                    <strong className="text-slate-900 font-medium dark:text-slate-200">
+                      {video.author ?? ""}
+                    </strong>
+                    <span className="text-slate-500 font-medium dark:text-slate-400">
+                      {(video.subCountText ?? "") + " Subscribers"}
+                    </span>
+                  </div>
+                </div>
               </div>
+            </div>
+            <div className="w-full lg:w-4/12 pl-10 -mt-3">
+              {video.recommendedVideos &&
+                video.recommendedVideos.map((video: any, i: number) => (
+                  <Link to={`/watch?v=${video.videoId}`} key={i}>
+                    <div className="flex items-center gap-3 mt-3">
+                      <img
+                        className="w-[168px] h-[94px] rounded-md"
+                        src={video.videoThumbnails[0]?.url ?? ""}
+                      ></img>
+                      <div className="flex flex-col">
+                        <strong className="text-slate-900 font-medium dark:text-slate-200">
+                          {video.title ?? ""}
+                        </strong>
+                        <span className="text-slate-500 font-medium dark:text-slate-400">
+                          {video.author ?? ""}
+                        </span>
+                        <span className="text-slate-600 text-sm dark:text-slate-500">
+                          {(video.viewCountText ?? "") + " Views"}
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
             </div>
           </div>
         </div>
